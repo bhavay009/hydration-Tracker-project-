e2d53250f712574d14d735e6872f0ed7b50d6e99
bhavay goyal <bhavaygoyal1@gmail.com>
2025-07-06 22:54:50 +0530

    home page

--- hydration-tracker/src/pages/Home.jsx
+++ hydration-tracker/src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Home.css';
 
 const Home = () => {
-  return (
-    <div>H</div>
-  )
-}
+  const [intake, setIntake] = useState(0);
+  const [goal, setGoal] = useState(2000);
+  const progress = Math.min((intake / goal) * 100, 100).toFixed(0);
 
-export default Home
+  return (
+    <div className="app">
+      <h1>Hydro Daily</h1>
+      <p>Track your daily hydration</p>
+
+      <div className="box">
+        <div className="row">
+          <span>Progress</span>
+          <span>{progress}%</span>
+        </div>
+        <div className="bar">
+          <div className="fill" style={{ width: `${progress}%` }}></div>
+        </div>
+        <div className="row">
+          <span>0 ml</span>
+          <span>{goal} ml</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
