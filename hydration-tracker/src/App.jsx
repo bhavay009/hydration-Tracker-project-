83f68944108f3c900d331e685776f34bccfe5902
bhavay goyal <bhavaygoyal1@gmail.com>
2025-07-06 17:23:53 +0530

    Added footer links and routing

--- hydration-tracker/src/App.jsx
+++ hydration-tracker/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Track from './pages/Track';
+import Reminder from './pages/Reminder';
+import Stats from './pages/stats';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/track" element={<Track />} />
+          <Route path="/reminder" element={<Reminder />} />
+          <Route path="/stats" element={<Stats />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
-export default App
+export default App;
